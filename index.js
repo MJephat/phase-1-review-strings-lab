fe7924fbb21f3a810c46239ff5bae8a38404571d
MJephat <mukundijephat22@gmail.com>
2023-03-19 10:49:36 +0300

    completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+// creating a variable and assign it to a value.
+const currentUser = "John Brisco";
+
+// adding two stings to make 1 longer through interpolation.
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//changinging from lower case to upper case(use js method)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//shortening greeting 
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+console.log( welcomeMessage);
+console.log( excitedWelcomeMessage);
+console.log( shortGreeting);
+
